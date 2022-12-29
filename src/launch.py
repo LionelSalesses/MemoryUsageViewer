8a73b4ebafd26c2ba514c8b68787faa665ea1f41
Lionel Salesses <l.salesses@gmail.com>
2022-12-29 12:35:08 +0100

    Improve launch script

--- src/launch.py
+++ src/launch.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+import sys
 from PyQt5.QtWidgets import QApplication
 from MemoryUsageViewer import MemoryUsageViewer
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     w = MemoryUsageViewer(app)
     w.show()
     app.exec_()
